c1b07f3c0fa9be6073b930ea445a707591380e41
nms377 <nms377@nyu.edu>
2017-03-17 18:30:00 -1000

    Setup my server.

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
 const express = require('express');
 const app = express();
+const env = require('dotenv').config();
+const https = require('https');
 
 app.use(express.static('public'));
+
+const {USER_ID, ACCESS_TOKEN} = process.env;
+
+let targetURL = `https://api.instagram.com/v1/users/${USER_ID}/media/recent/?count=99&access_token=${ACCESS_TOKEN}`;
+
+app.get('/api/instafeed', function (req, res){
+	getPhotos(targetURL)
+		.then((images) => {
+			res.json(JSON.parse(images));
+		})
+		.catch((err) => {
+			console.log('err', err);
+			res.send(err);
+		});
+});
+
+function getPhotos(endpoing) {
+	return new Promise((resolve, reject) => {
+		https.get(endpoing, res => {
+			let rawData = '';
+			res.on('data', chunk => rawData += chunk);
+			res.on('end', () => {
+				resolve(rawData);
+			});
+		})
+		.on('err', err => reject(err));
+	});
+}
 
 app.listen(9000, function() {
 	console.log('Server listening on port 9000');
